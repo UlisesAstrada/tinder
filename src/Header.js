4f9fb12c50e4067da4351c5cecb7a61112d368a5
Ulises <ulises_astrada@outlook.com>
2021-01-14 17:28:09 -0300

    adding ForumIcon to Header

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './Header.css'
 import PersonIcon from '@material-ui/icons/Person';
 import IconButton from '@material-ui/core/IconButton';
+import ForumIcon from '@material-ui/icons/Forum'
+
 
 function Header() {
   return (
@@ -15,6 +17,11 @@ function Header() {
         src='https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png'
         alt='tinder logo'
         />
+      
+      <IconButton>
+        <ForumIcon fontSize='large' className='header__icon' />
+      </IconButton>
+
     </div>
   )
 }
