05b3e02433a4753cc6b11b6261c20fb8fc093065
Ulises <ulises_astrada@outlook.com>
2021-01-14 18:27:36 -0300

    adding TinderCards.js and TinderCards.css

---
+++ src/TinderCards.js
@@ -0,0 +1,19 @@
+import React { useState } from 'react'
+import './TinderCards.css'
+
+
+function TinderCards() {
+  const [people, setPeople] = useState([
+    {
+      
+    }
+  ]);
+
+  return (
+    <div className='tinderCards'>
+
+    </div>
+  )
+}
+
+export default TinderCards
