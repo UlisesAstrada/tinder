22a81bad293bf96c0e584dc9c985e7c1a077fc62
Ulises <ulises_astrada@outlook.com>
2021-01-14 17:10:03 -0300

    adding Header.css

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import './Header.css'
 import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton';
 
 function Header() {
   return (
     <div className='header'>
-      <h2>I am a header and also my creator is a genius</h2>
-      <PersonIcon />
+      <IconButton>
+        <PersonIcon fontSize='large' className='header__icon' />
+      </IconButton>
+
+      <img 
+        className='header__logo'
+        src='https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png'
+        alt='tinder logo'
+        />
     </div>
   )
 }
